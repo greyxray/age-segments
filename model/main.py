b094afd7e4e550764974ab07928a84e8cf485a4c
greyxray <greyxray@gmail.com>
2021-03-21 16:16:02 +0100

    add sample weights option

--- model/main.py
+++ model/main.py
@@ -7,18 +7,35 @@ from sklearn.metrics import classification_report
 from model.build import build_model
 
 
+SAMPLE_WEIGHTS = {
+    # Control the sample weights by this mapping
+    # "20-29": 1, -- the main solution uses this class weight
+    "20-29": 5,
+    "30-39": 1,
+    "40-49": 1,
+    "50-59": 1,
+    "60+": 1,
+}
+
+
 @click.command()
 @click.option("--data", type=click.Path(exists=True),
               default="data/data.csv")
 def main(data):
     df = pd.read_csv(data).fillna("UNK")
+    df["sample_weight"] = df["age"].map(SAMPLE_WEIGHTS)
 
     provider1 = df[df["provider"] == "provider1"]
     provider2 = df[df["provider"] == "provider2"]
 
     X_tr, X_te, y_tr, y_te = train_test_split(provider1, provider1["age"])
     model = build_model()
-    model.fit(X_tr, y_tr)
+    model.fit(
+        X_tr,
+        y_tr,
+        logisticregression__sample_weight=X_tr["sample_weight"]
+    )
+
 
     print("Done fitting")
 
